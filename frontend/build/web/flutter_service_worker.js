9868a1b88c03ee7dd6a1bbab8469c60c349b878e
makrung <makrung2@gmail.com>
2025-11-25 18:24:35 +0700

    chore(web): sync build folder flutter bootstrap and service worker

--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "713b1bbd1b8623a096d876f7ed71d5d1
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "775e66f5fd9a19a9f4deb123017b1b0b",
+"flutter_bootstrap.js": "2f9c088cc5d4f2982b914cb6ffb5eeba",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
